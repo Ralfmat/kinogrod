2cc24df4e8d32adce5f9161c47f9b4adb773fce3
Ralfmat <mateuszwanda@outlook.com>
2023-05-10 19:46:36 +0200

    Update

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿// using Microsoft.EntityFrameworkCore;
+
+// namespace kinoUI.Data;
+
+// public class DatabaseContext : DbContext
+// {
+
+//     public DatabaseContext(Parameters)
+//     {
+        
+//     }
+
+
+//     public DbSet<Film> Film { get; set; }
+//     public DbSet<FilmDetails> FilmDetails { get; set; }
+// }
---
+++ Models/FilmDetails.cs
@@ -0,0 +1,11 @@
+namespace kinoUI.Models;
+
+public class FilmDetails
+{
+    public int Id { get; set; }
+    
+    public string Description { get; set; }
+
+    public int LengthInMinutes { get; set; }
+
+}
