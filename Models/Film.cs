6b9eaa1b45a7a3cedda5265fea8393f72e953023
Mateusz <mateuszwanda@outlook.com>
2023-05-23 14:29:55 +0200

    footer UI

--- Models/Film.cs
+++ Models/Film.cs
@@ -3,15 +3,10 @@ namespace kinoUI.Models;
 public class Film
 {
     public int Id { get; set; }
-
     public string Title { get; set; }
-
     public string? OriginalTitle { get; set; }
-
     public int? ReleaseYear { get; set; }
-
     public string? ImageUrl { get; set; }
-
     public string? FilmGenre { get; set; }
     public string? Description { get; set; }
     public int? LengthInMinutes { get; set; }
